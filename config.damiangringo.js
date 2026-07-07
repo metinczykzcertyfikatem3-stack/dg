e664fe9701506fb6d3f29b1e138fe8405f4ceca0
metinczykzcertyfikatem3-stack <metinczykzcertyfikatem3@gmail.com>
2026-07-08 00:40:17 +0200

    Update config.damiangringo.js

--- config.damiangringo.js
+++ config.damiangringo.js
@@ -14,15 +14,26 @@ window.SITE = {
   payments: ["BLIK", "Visa / Mastercard", "Kryptowaluty", "Portfele wirtualne"],
 
   casinos: [
+    
+    { name: "Leon", logo: "assets/leon.png",
+      link: "https://dg-leon.lovable.app/",
+      bonus: '<span class="g">12 000zł</span> w <b>Bonusach Powitalnych</b>,
+      minDep: "50 zł" },
+    
+      { name: "Slotuna", logo: "assets/slotuna.jpeg",
+      link: "https://cocoeschwa.com/api/v3/offer/578?affiliate_id=199&url_id=683",
+      bonus: '<span class="g">100%</span> do <b>2250 zł</b> + <b>200</b> darmowych spinów + 1 bonus crab',
+      minDep: "50 zł" },
+
+    { name: "Mr Punter", logo: "assets/mrpunter.png",
+      link: "https://mrp.vordyne.com/?mid=376797_2181728",
+      bonus: '<span class="g">100%</span> do <b>2 250 zł</b> + <b>200</b> darmowych spinów',
+      minDep: "50 zł" },
+    
     { name: "BettySpin", logo: "assets/bettyspin.png",
       link: "https://cocoeschwa.com/api/v3/offer/597?affiliate_id=199&url_id=718",
       bonus: '<span class="g">100-150%</span> do <b>7500 zł</b> + <b>50-150</b> darmowych spinów',
       minDep: "80 zł (bonus od min. wpłaty 100 zł)" },
-
-    { name: "Slotuna", logo: "assets/slotuna.jpeg",
-      link: "https://cocoeschwa.com/api/v3/offer/578?affiliate_id=199&url_id=683",
-      bonus: '<span class="g">100%</span> do <b>2250 zł</b> + <b>200</b> darmowych spinów + 1 bonus crab',
-      minDep: "50 zł" },
 
     { name: "JasminSlots", logo: "assets/jasmin-slots.jpeg",
       link: "https://cocoeschwa.com/api/v3/offer/598?affiliate_id=199&url_id=719",
@@ -35,11 +46,6 @@ window.SITE = {
       note: "lub do 400% do 8330 zł + 150 FS (rozłożone na 3 pierwsze wpłaty)",
       minDep: "100 zł" },
 
-    { name: "Reel Raven", logo: "assets/reel-raven.png",
-      link: "https://cocoeschwa.com/api/v3/offer/523?affiliate_id=199&url_id=536",
-      bonus: '<span class="g">350%</span> do <b>2000 €</b> + <b>1000</b> darmowych spinów',
-      minDep: "20 €" },
-
     { name: "Zoccer", logo: "assets/zoccer.png",
       link: "https://cocoeschwa.com/api/v3/offer/601?affiliate_id=199&url_id=722",
       bonus: '<span class="g">100%</span> do <b>2250 zł</b> + <b>200</b> darmowych spinów',
@@ -47,10 +53,9 @@ window.SITE = {
   ],
 
   disclaimer:
-    "<strong>Strona nie przeznaczona dla osób znajdujących się w Polsce, legalny dostęp możliwy jedynie dla Polaków przebywających poza granicami tego kraju.</strong> " +
-    "Materiał ma charakter informacyjny i afiliacyjny. Ta strona nie jest operatorem gier hazardowych " +
-    "i może otrzymywać wynagrodzenie za rejestracje dokonane przez zamieszczone linki. Prezentowane bonusy " +
-    "podlegają regulaminom i warunkom obrotu poszczególnych kasyn. Szczegóły sprawdź u operatora. " +
-    "Hazard może uzależniać. Jeśli przestaje być rozrywką, szukaj pomocy. Telefon zaufania: " +
-    '<a href="tel:801889880">801 889 880</a> (uzaleznieniabehawioralne.pl).'
+    "<strong>Uwaga: serwis niedostępny dla użytkowników znajdujących się w Polsce.</strong> " +
+    "Dostęp możliwy jedynie dla osób przebywających poza granicami kraju. " +
+    "Wchodząc na stronę, deklarujesz zgodność z tym wymogiem. " +
+    "Potrzebujesz pomocy? Odwiedź " +
+    '<a href=https://www.gamblersanonymous.org/>Anonymous Gamblers</a>.'
 };
